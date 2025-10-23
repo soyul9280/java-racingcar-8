9c46e5dd535ae6464f496a76935653edf289a4e6
soyul <soyul9280@gmail.com>
2025-10-23 23:26:07 +0900

    feat: 시도횟수가 0인 경우 예외 처리

--- src/main/java/racingcar/model/AttemptNumber.java
+++ src/main/java/racingcar/model/AttemptNumber.java
@@ -9,6 +9,9 @@ public record AttemptNumber(int attemptNumber) {
     }
 
     private void validate(int input) {
+        if (attemptNumber == 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ATTEMPT_NUMBER_ZERO.message());
+        }
         if (input < 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_ATTEMPT_NUMBER_MINUS.message());
         }
