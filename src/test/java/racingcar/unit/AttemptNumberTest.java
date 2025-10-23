780242ef3910993e87d607f72147d5fc92dc2c8c
soyul <soyul9280@gmail.com>
2025-10-23 23:28:10 +0900

    refactor: AttemptNumberTest Refactor 단계 진행
    - 필요한 AttemptNumber클래스 import
    - ErrorMessage에서 message()처리
    - 현상: 생성자로 점검할 경우 int가 파라미터기 떄문에 null점검 못함
    - 해결: create함수 생성"

--- src/test/java/racingcar/unit/AttemptNumberTest.java
+++ src/test/java/racingcar/unit/AttemptNumberTest.java
@@ -8,40 +8,41 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.message.ErrorMessage;
+import racingcar.model.AttemptNumber;
 
 public class AttemptNumberTest {
 
     @Test
     @DisplayName("예외: 시도 횟수가 0인 경우")
     void InvalidAttemptNumber_Zero_Fail() {
-        assertThatThrownBy(() -> new AttemptNumber(0))
+        assertThatThrownBy(() -> AttemptNumber.create("0"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_ZERO);
+                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_ZERO.message());
     }
 
     @Test
     @DisplayName("예외: 시도 횟수가 null인 경우")
     void InvalidAttemptNumber_Null_Fail() {
-        assertThatThrownBy(()-> new AttemptNumber(null))
+        assertThatThrownBy(()-> AttemptNumber.create(null))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_NULL);
+                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_NULL.message());
     }
 
     @ParameterizedTest(name = "[case: {index}]")
     @ValueSource(strings = {""," "})
     @DisplayName("예외: 시도 횟수가 빈 문자열 혹은 공백인 경우")
     void InvalidAttemptNumber_Blank_Fail(String name) {
-        assertThatThrownBy(()-> new AttemptNumber(name))
+        assertThatThrownBy(()-> AttemptNumber.create(name))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_BLANK);
+                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_BLANK.message());
     }
 
     @Test
     @DisplayName("예외: 시도 횟수가 0 미만인 경우")
     void InvalidAttemptNumber_Minus_Fail() {
-        assertThatThrownBy(()-> new AttemptNumber(-2))
+        assertThatThrownBy(()-> AttemptNumber.create("-2"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_MINUS);
+                .hasMessage(ErrorMessage.INVALID_ATTEMPT_NUMBER_MINUS.message());
     }
 
 }
