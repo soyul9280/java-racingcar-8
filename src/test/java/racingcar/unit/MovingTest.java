53eb8d329820f23ac8e8128f70a1eba968f8be5c
soyul <soyul9280@gmail.com>
2025-10-24 11:37:59 +0900

    refactor: 이동테스트 Refactor단계 진행
    - 결과 비교 방법 boolean으로 변경

--- src/test/java/racingcar/unit/MovingTest.java
+++ src/test/java/racingcar/unit/MovingTest.java
@@ -9,18 +9,6 @@ import racingcar.model.Car;
 import racingcar.model.CarName;
 
 public class MovingTest {
-    @ParameterizedTest(name = "[case : {0}]")
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @DisplayName("정상: 4 이상으로 움직이는 경우")
-    void moveForward_Go_Success(int randomNumber) {
-        //given
-        Car car = new Car(new CarName("Ayo"));
-        //when
-        car.moveForward(randomNumber);
-        //then
-        assertThat(car.position).isEqualTo(1);
-    }
-
     @ParameterizedTest(name = "[case: {0}]")
     @ValueSource(ints = {0, 1, 2, 3})
     @DisplayName("정상: 4 미만으로 움직이지 않는 경우")
@@ -30,9 +18,18 @@ public class MovingTest {
         //when
         car.moveForward(randomNumber);
         //then
-        assertThat(car.position).isEqualTo(0);
-
+        assertThat(car.comparesPosition(0)).isEqualTo(true);
     }
 
-
+    @ParameterizedTest(name = "[case : {0}]")
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("정상: 4 이상으로 움직이는 경우")
+    void moveForward_Go_Success(int randomNumber) {
+        //given
+        Car car = new Car(new CarName("Ayo"));
+        //when
+        car.moveForward(randomNumber);
+        //then
+        assertThat(car.comparesPosition(1)).isEqualTo(true);
+    }
 }
